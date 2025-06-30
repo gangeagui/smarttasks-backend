285d03c85674c8ded4decb7146fe0c64f1a505ae
Gustavo Aguilar <gangeagui@gmail.com>
2025-06-30 10:16:22 -0600

    Primer commit - proyecto base Spring Boot

---
+++ src/test/java/com/gangeagui/smarttasks/SmarttasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangeagui.smarttasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarttasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
