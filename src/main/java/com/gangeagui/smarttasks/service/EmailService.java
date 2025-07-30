c44e56ff79366885077498c0defa94b1b4a7581d
Gustavo Aguilar <gangeagui@gmail.com>
2025-07-30 13:31:28 -0600

    nuevos servicios para realizar el envio de correo y el cambio de la contraseña

--- src/main/java/com/gangeagui/smarttasks/service/EmailService.java
+++ src/main/java/com/gangeagui/smarttasks/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.gangeagui.smarttasks.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,18 +7,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String from;
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
-    public void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(body);
+        mailSender.send(msg);
     }
 }
