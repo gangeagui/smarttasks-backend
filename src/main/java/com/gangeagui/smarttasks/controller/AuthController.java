8a9ec3c24e51af09b04316ace017fc05d384eec3
Gustavo Aguilar <gangeagui@gmail.com>
2025-07-30 13:21:19 -0600

    ajustes de identacion y separacion de un servicio

--- src/main/java/com/gangeagui/smarttasks/controller/AuthController.java
+++ src/main/java/com/gangeagui/smarttasks/controller/AuthController.java
@@ -6,14 +6,11 @@ import com.gangeagui.smarttasks.exception.ResourceNotFoundException;
 import com.gangeagui.smarttasks.model.User;
 import com.gangeagui.smarttasks.repository.UserRepository;
 import com.gangeagui.smarttasks.security.JwtService;
-import com.gangeagui.smarttasks.service.PasswordResetService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -28,9 +25,6 @@ public class AuthController {
     @Autowired
     private JwtService jwtService;
 
-    @Autowired
-    private PasswordResetService passwordResetService;
-
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
@@ -42,12 +36,5 @@ public class AuthController {
 
         String token = jwtService.generateToken(user);
         return ResponseEntity.ok(new AuthResponse(token));
-    }
-
-    @PostMapping("/forgot-password")
-    public ResponseEntity<?> forgotPassword(@RequestBody Map<String, String> body) {
-        String email = body.get("email");
-        passwordResetService.sendPasswordResetToken(email);
-        return ResponseEntity.ok("Correo de recuperación enviado");
     }
 }
--- src/main/java/com/gangeagui/smarttasks/exception/ResourceNotFoundException.java
+++ src/main/java/com/gangeagui/smarttasks/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.gangeagui.smarttasks.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException (String message) {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
